f81a1f28b837395de341a3fdf3fa7f4e256ce6ba
Erik Rodner <Erik.Rodner@uni-jena.de>
2013-11-16 14:30:51 +0100

    script to create list of ilsvrc categ. from synsets

---
+++ create-imagenet-category-list.py
@@ -0,0 +1,53 @@
+from imagenet_analysis import ImageNetData
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument( '-i', '--inputsynsets', help='list of synsets', default='categories.synset')
+parser.add_argument( '-d', '--datadir', help='ILSVRC devkit data directory', default='/home/dbv/bilder/ilsvrc2012/devkit-1.0/data/')
+parser.add_argument( '-y', '--year', help='ILSVRC year', default='2012' )
+parser.add_argument( '-o', '--outfn', help='output file name (JSON format)', default='categories.json' )
+parser.add_argument( '-t', '--desctype', help='type of the category descriptions in the output', default='child', choices=['root', 'child'] )
+parser.add_argument( '-s', '--shorten', help='shorten description', default=True )
+
+args = parser.parse_args()
+synsetlistfn = args.inputsynsets
+imnet = ImageNetData( args.datadir, ilsvrcyear=args.year)
+outfn = args.outfn
+shortendesc = args.shorten
+
+with open(synsetlistfn,'r') as synsetlist:
+  ids = {}
+
+  for wnid in synsetlist:
+    try:
+      wnid = wnid.rstrip()
+      idx = imnet.class_idx_from_wnid(wnid)
+    except:
+      print "# %s does not exists!" % (wnid)
+      continue
+
+
+    idxchilds = imnet.get_leafs(idx)
+    rootdesc = imnet.description_from_class_idx( idx )
+    print "# %s (%s)" % (wnid, rootdesc )
+    for idxchild in idxchilds:
+      childdesc = imnet.description_from_class_idx(idxchild)
+      print "# -> %s" % ( childdesc )
+      id = imnet.imagenet_id_from_class_idx ( idxchild )
+      print "%d" % (id)
+
+      
+      if args.desctype == 'root':
+        desc = rootdesc
+      else:
+        desc = childdesc
+
+      if shortendesc:
+        descitems = desc.split(',')
+        desc = descitems[0].rstrip()
+
+      ids[str(id)] = desc
+
+with open (outfn, 'w') as outfile:
+  json.dump ( ids, outfile, sort_keys=False, indent=4 )
